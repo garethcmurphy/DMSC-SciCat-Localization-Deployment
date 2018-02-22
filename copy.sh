b2e21e4ea3c14089be98c816a70a53f21156de43
Gareth Murphy <garethmurphy@scicat01.esss.lu.se>
2018-02-22 09:36:59 +0100

    update copy and env

--- copy.sh
+++ copy.sh
@@ -1,10 +1,13 @@
-\cp    ../dmsc_scicat_localization/run.sh .
-\cp    ../dmsc_scicat_localization/start.sh .
-\cp    ../dmsc_scicat_localization/rabbit.yaml .   
-\cp    ../dmsc_scicat_localization/mongo.yaml .   
-\cp ../dmsc_scicat_localization/catamel/deployment.yaml  services/catamel/dacat-api-server/templates/deployment.yaml
-\cp ../dmsc_scicat_localization/catamel/ingress.yaml     services/catamel/dacat-api-server/templates/ingress.yaml
-\cp ../dmsc_scicat_localization/catamel/values.yaml    services/catamel/dacat-api-server/values.yaml
-\cp ../dmsc_scicat_localization/catanie/NOTES.txt    services/catanie/dacat-gui/templates/NOTES.txt
-\cp ../dmsc_scicat_localization/catanie/ingress.yaml   services/catanie/dacat-gui/templates/ingress.yaml
-\cp ../dmsc_scicat_localization/catanie/values.yaml    services/catanie/dacat-gui/values.yaml
+DMSC_PATH='../dmsc_scicat_localization'
+\cp ${DMSC_PATH}/run.sh .
+\cp ${DMSC_PATH}/start.sh .
+\cp ${DMSC_PATH}/rabbit.yaml .   
+\cp ${DMSC_PATH}/mongo.yaml .   
+\cp ${DMSC_PATH}/catamel/deployment.yaml  services/catamel/dacat-api-server/templates/deployment.yaml
+\cp ${DMSC_PATH}/catamel/ingress.yaml     services/catamel/dacat-api-server/templates/ingress.yaml
+\cp ${DMSC_PATH}/catamel/values.yaml    services/catamel/dacat-api-server/values.yaml
+\cp ${DMSC_PATH}/catanie/NOTES.txt    services/catanie/dacat-gui/templates/NOTES.txt
+\cp ${DMSC_PATH}/catanie/ingress.yaml   services/catanie/dacat-gui/templates/ingress.yaml
+\cp ${DMSC_PATH}/catanie/values.yaml    services/catanie/dacat-gui/values.yaml
+\cp ${DMSC_PATH}/environment.ts services/catanie/component/src/environments/
+\cp ${DMSC_PATH}/favicon.ico services/catanie/component/src/
